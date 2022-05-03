56453ae81460f86ea0e348fc10675a83dde09904
Alfredo <alfredo.loera01@utrgv.edu>
2022-05-03 11:54:53 -0500

    updates

--- lib/Weight.dart
+++ lib/Weight.dart
@@ -68,8 +68,31 @@ primaryYAxis: CategoryAxis(
         WeightData(4, 125),
         WeightData(5, 130),
         WeightData(6, 140),
-        
-        
+        WeightData(7, 135),
+        WeightData(8, 137),
+        WeightData(9, 130),
+        WeightData(10, 125),
+        WeightData(11, 130),
+        WeightData(12, 140),
+        WeightData(13, 135),
+        WeightData(14, 137),
+        WeightData(15, 130),
+        WeightData(16, 125),
+        WeightData(17, 130),
+        WeightData(18, 140),
+        WeightData(19, 125),
+        WeightData(20, 130),
+        WeightData(21, 140),
+        WeightData(22, 135),
+        WeightData(23, 137),
+        WeightData(24, 130),
+        WeightData(25, 125),
+        WeightData(26, 130),
+        WeightData(27, 140),
+        WeightData(28, 130),
+        WeightData(29, 125),
+        WeightData(30, 130),
+        WeightData(31, 140),
       ];
       return charData; 
     }
